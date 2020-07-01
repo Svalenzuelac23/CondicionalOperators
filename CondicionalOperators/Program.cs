bb63e3c089b0c3b01f8cfc5fe09654ff02a6d037
Stanley Valenzuela <s.valenzuela@limenainc.net>
2020-07-01 17:22:04 -0600

    Add project files.

---
+++ CondicionalOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CondicionalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //documentacion
+            //https://docs.microsoft.com/es-es/dotnet/csharp/language-reference/operators/member-access-operators#null-conditional-operators--and-
+
+            double sumar(List<double[]> numeros, int indexaSumar)
+            {
+                return numeros?[indexaSumar]?.Count() ?? double.NaN;
+            }
+
+            var suma1 = sumar(null, 0);
+
+            var numerosLista = new List<double[]>
+            {
+                new[] {1.0,5.0,2.0,3.0,1.0},
+                new[] {1.0,2.0},
+                null
+            };
+
+            var suma2 = sumar(numerosLista, 1);
+
+            var suma3 = sumar(numerosLista, 2); 
+            
+            Console.WriteLine(suma1);
+            Console.WriteLine(suma2);
+            Console.WriteLine(suma3);
+            Console.Read();
+        }
+    }
+}
